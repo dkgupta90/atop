8cbc170a1dbe9f62c751836dd62cc13bc5cf8b87
Deepak Gupta <me@deepakgupta.net.in>
2018-05-01 21:24:08 +0530

    boundary_values corrected for 3D, still wrong output displacements in y and zz

--- atop/source/fem/boundary_values.cpp
+++ atop/source/fem/boundary_values.cpp
@@ -52,63 +52,80 @@ void BoundaryValues<dim>::vector_value(const Point<dim> &p,
 			ExcDimensionMismatch(values.size(), dim));
 	Assert(dim >= 2,
 			ExcNotImplemented());
-	unsigned int model_problem = 3;
+
 	double xmin, xmax, ymin, ymax;
+	double zmin, zmax;
+	if (dim == 2){
+		unsigned int model_problem = 3;
 
-	if (model_problem == 4){
-		xmin = 0, ymin = 0, xmax = 2, ymax = 1;
-		if (std::fabs(p(1) - (ymax)) < 1e-12){
-			values(0) = 0;
-			values(1) = 0;
+
+		if (model_problem == 4){
+			xmin = 0, ymin = 0, xmax = 2, ymax = 1;
+			if (std::fabs(p(1) - (ymax)) < 1e-12){
+				values(0) = 0;
+				values(1) = 0;
+			}
+		}
+		else if (model_problem == 5){
+			//cantilever problem
+			xmin = 0, ymin = 0, xmax = 1, ymax = 1;
+			if (std::fabs(p(0) - (ymin)) < 1e-12){
+				values(0) = 1;
+				values(1) = 0;
+			}
+		}
+		else if (model_problem == 3){
+			//cantilever problem
+			xmin = 0, ymin = 0, xmax = 2, ymax = 1;
+			if (std::fabs(p(0) - (xmin)) < 1e-12){
+				values(0) = 0;
+				values(1) = 0;
+			}
+		}
+		else if (model_problem == 2) {
+			//Mitchel stress problem
+			xmin = 0, ymin = 0, xmax = 1.5, ymax = 1;
+			double rball = 0.02;
+			Point<dim> center;
+			center(0) = xmin + rball;
+			center(1) = (ymax+ymin)/2;
+			if (center.distance(p) <= rball){
+				values(0) = 0;
+				values(1) = 0;
+			}
+		}
+		//below one has to be checked, looks to be wrong
+		else if (model_problem == 1){
+			//MBB problem
+			xmin = 0, ymin = 0, xmax = 1, ymax = 1;
+			if (sqrt(pow(p[0] - 0.5, 2) + pow(p[1] - 0.5, 2)) < 0.027){
+				values(0) = 0;
+				values(1) = 0;
+			}
+		}
+
+		//force invertor problem
+		else if (model_problem == 6){
+			//MBB problem
+			xmin = 0, ymin = 0, xmax = 2, ymax = 1;
+			if (std::fabs(p(0) - (xmin)) < 1e-12 && (p(1) - 0.05) < 0){
+				values(0) = 0;
+				values(1) = 0;
+			}
 		}
 	}
-	else if (model_problem == 5){
-		//cantilever problem
-		xmin = 0, ymin = 0, xmax = 1, ymax = 1;
-		if (std::fabs(p(0) - (ymin)) < 1e-12){
-			values(0) = 1;
-			values(1) = 0;
-		}
-	}
-	else if (model_problem == 3){
-		//cantilever problem
-		xmin = 0, ymin = 0, xmax = 2, ymax = 1;
+	else if (dim == 3){
+		// Defining the boundaries for the current 3D case
+		xmin = 0, ymin = 0, zmin = 0;
+		xmax = 4, ymax = 2, zmax = 1;
 		if (std::fabs(p(0) - (xmin)) < 1e-12){
+			std::cout<<"Reached here in boundary values"<<std::endl;
 			values(0) = 0;
 			values(1) = 0;
-		}
-	}
-	else if (model_problem == 2) {
-		//Mitchel stress problem
-		xmin = 0, ymin = 0, xmax = 1.5, ymax = 1;
-		double rball = 0.02;
-		Point<dim> center;
-		center(0) = xmin + rball;
-		center(1) = (ymax+ymin)/2;
-		if (center.distance(p) <= rball){
-			values(0) = 0;
-			values(1) = 0;
-		}
-	}
-	//below one has to be checked, looks to be wrong
-	else if (model_problem == 1){
-		//MBB problem
-		xmin = 0, ymin = 0, xmax = 1, ymax = 1;
-		if (sqrt(pow(p[0] - 0.5, 2) + pow(p[1] - 0.5, 2)) < 0.027){
-			values(0) = 0;
-			values(1) = 0;
+			values(2) = 0;
 		}
 	}
 
-	//force invertor problem
-	else if (model_problem == 6){
-		//MBB problem
-		xmin = 0, ymin = 0, xmax = 2, ymax = 1;
-		if (std::fabs(p(0) - (xmin)) < 1e-12 && (p(1) - 0.05) < 0){
-			values(0) = 0;
-			values(1) = 0;
-		}
-	}
 }
 
 template <int dim>
